469de44346665f8b068a22e4d3f67f1cce53a8e1
dongwoo2 <ieiedong@naver.com>
2024-01-02 20:29:47 +0900

    20240102 파이선공부 1~2

---
+++ day01/exam01.py
@@ -0,0 +1,8 @@
+# 주석(comment)
+print('숫자 입력 :', end = '')
+n1 = input()
+n1 = int(n1)
+n2 = int(input('숫자 입력:'))
+
+result = n1 + n2
+print('결과는',result)
---
+++ day01/exam02.py
@@ -0,0 +1,10 @@
+print(100,10,'hello', sep = '!!!', end = '~~')
+print('hello' + 'world')
+print(3.14)
+print(True)
+
+print("\"i am adult!\"")
+print('"i am adult!"')
+
+print('I\'m a adult')
+print("I'm a adult!")
